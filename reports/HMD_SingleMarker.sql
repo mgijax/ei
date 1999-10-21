920c1a03c47e5912bada639a133c529cbf9abe66
Lori Corbani <Lori.Corbani@jax.org>
1999-10-21 11:53:42 +0000

    tr 989

---
+++ reports/HMD_SingleMarker.sql
@@ -0,0 +1,70 @@
+#!/bin/csh
+ 
+setenv DSQUERY $1
+setenv MGD $2
+ 
+header.sh $0
+
+isql -S$DSQUERY -Umgd_public -Pmgdpub -w200 <<END >> $HOME/mgireport/$0.rpt
+
+use $MGD 
+go
+
+set nocount on
+go
+
+select h._Class_key, h._Refs_key, m._Homology_key
+into #homology
+from HMD_Homology h, HMD_Homology_Marker m
+where h._Homology_key = m._Homology_key
+go
+
+select *
+into #delete
+from #homology
+group by _Class_key, _Refs_key
+having count(*) > 1
+go
+
+delete #homology
+from #delete d, #homology h
+where d._Class_key = h._Class_key
+go
+
+drop table #delete
+go
+
+select *
+into #delete
+from #homology
+group by _Class_key
+having count(*) = 1
+go
+
+delete #homology
+from #delete d, #homology h
+where d._Class_key = h._Class_key
+go
+
+select distinct _Class_key
+into #homology1
+from #homology
+go
+
+set nocount off
+go
+
+print ""
+print "Homologies defined by Marker Symbol only; No Common Reference; No Single Homologies"
+print ""
+
+select r.symbol, r.commonName, r.jnum, r._Class_key
+from #homology1 h, HMD_Homology_View r
+where h._Class_key = r._Class_key
+order by r._Class_key, r.symbol
+go
+ 
+quit
+
+END
+
