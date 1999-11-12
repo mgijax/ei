26e288743f05af8836eb25342a8a212d0d6e57ec
Lori Corbani <Lori.Corbani@jax.org>
1999-11-12 20:43:49 +0000

    tr1075

---
+++ reports/HMD_ConservedUN.sql
@@ -0,0 +1,44 @@
+#!/bin/csh
+ 
+#
+# TR 1075 - Homologies w/ Conserved Location and Chromosome UN
+#
+# Template for SQL report
+#
+# Notes:
+#	- all public reports require a header and trailer
+#	- all private reports require a header
+#
+# Usage:
+#	HMD_ConservedUN.sql MGD mgd
+#
+
+
+setenv DSQUERY $1
+setenv MGD $2
+
+header.sh $0
+
+isql -S$DSQUERY -Umgd_public -Pmgdpub -w200 <<END >> $0.rpt
+
+use $MGD
+go
+
+print ""
+print "Homologies w/ Conserved Map Location and Chromosome = UN"
+print ""
+
+select h.symbol, h.species, h.chromosome
+from HMD_Homology_View h, HMD_Homology_Assay a
+where h._Homology_key = a._Homology_key
+and a._Assay_key = 14
+and h.chromosome = "UN"
+order by h._Homology_key, h.symbol
+go
+
+quit
+
+END
+
+cat trailer >> $HOME/mgireport/$0.rpt
+
