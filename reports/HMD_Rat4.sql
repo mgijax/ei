9e7b61ef9b4addcdbac5c7a9b1380b2963c51023
Lori Corbani <Lori.Corbani@jax.org>
1999-12-14 15:09:56 +0000

    use case expression

--- reports/HMD_Rat4.sql
+++ reports/HMD_Rat4.sql
@@ -10,23 +10,6 @@ isql -S$DSQUERY -Umgd_public -Pmgdpub -w200 <<END >> $HOME/mgireport/$0.rpt
 use $MGD 
 go
 
-set nocount on
-go
-
-select distinct _Marker_key, offset, offsetDisplay = str(offset, 10, 2)
-into #offsets
-from MRK_Offset where source = 0 and offset >= 0
-union
-select distinct _Marker_key, offset, offsetDisplay = "       N/A"
-from MRK_Offset where source = 0 and offset = -999.0
-union
-select distinct _Marker_key, offset, offsetDisplay = "  syntenic"
-from MRK_Offset where source = 0 and offset = -1.0
-go
- 
-set nocount off
-go
-
 print ""
 print "Homology - Rat vs. Mouse (Sorted by Mouse Symbol)"
 print ""
@@ -34,13 +17,22 @@ print ""
 select distinct 
 m2.symbol "Mouse-Symbol", 
 m2.chromosome "Mouse-Chr", 
-offsetDisplay "Mouse-cM Position", 
-m2.cytogeneticOffset "Mouse-Band",
+
+"Mouse-cM Position" =
+        case
+        when o.offset >= 0 then str(o.offset, 10, 2)
+        when o.offset = -999.0 then "       N/A"
+        when o.offset = -1.0 then "  syntenic"
+        end
+
+,m2.cytogeneticOffset "Mouse-Band",
 m1.symbol "Rat-Symbol",
 m1.chromosome + m1.cytogeneticOffset "Rat-Chr"
+
 from HMD_Homology r1, HMD_Homology_Marker h1,
 HMD_Homology r2, HMD_Homology_Marker h2,
-MRK_Marker m1, MRK_Marker m2, #offsets o, MRK_Chromosome c
+MRK_Marker m1, MRK_Marker m2, MRK_Offset o, MRK_Chromosome c
+
 where m1._Species_key = 40
 and m1._Marker_key = h1._Marker_key
 and h1._Homology_key = r1._Homology_key
@@ -49,6 +41,7 @@ and r2._Homology_key = h2._Homology_key
 and h2._Marker_key = m2._Marker_key
 and m2._Species_key = 1
 and m2._Marker_key = o._Marker_key
+and o.source = 0
 and m1._Species_key = c._Species_key
 and m1.chromosome = c.chromosome
 order by m2.symbol, m2.chromosome, m2.cytogeneticOffset
