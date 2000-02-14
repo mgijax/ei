1ba6ac2d392e8d0eea5397299e9c00616b1a00d6
Lori Corbani <Lori.Corbani@jax.org>
2000-02-14 16:21:14 +0000

    update CVS version

--- dsrc/MGILib.d
+++ dsrc/MGILib.d
@@ -66,7 +66,7 @@ rules:
 
 	  top := create widget("Login", nil, nil);
 
-	  global_version := "CVS 1-5-4";
+	  global_version := "CVS 1-5-5";
 
 	  SetTitle.source_widget := top;
 	  send(SetTitle, 0);
