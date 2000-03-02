72c0f411cc5a3945a451bc084dc298d114095179
Lori Corbani <Lori.Corbani@jax.org>
2000-03-02 13:52:55 +0000

    1-5-12

--- dsrc/MGILib.d
+++ dsrc/MGILib.d
@@ -66,7 +66,7 @@ rules:
 
 	  top := create widget("Login", nil, nil);
 
-	  global_version := "CVS 1-5-11";
+	  global_version := "CVS 1-5-12";
 
 	  SetTitle.source_widget := top;
 	  send(SetTitle, 0);
