1845fc6949f298bc2c4e71b1cc589a8b33a59f52
Lori Corbani <Lori.Corbani@jax.org>
2000-03-29 15:24:53 +0000

    reports moved to new product

--- reports/header.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-echo "The Jackson Laboratory - Mouse Genome Informatics - Mouse Genome Database (MGD)" > $HOME/mgireport/$1.rpt
-echo "Copyright 1996, 1999, 2000 The Jackson Laboratory" >> $HOME/mgireport/$1.rpt
-echo "All Rights Reserved" >> $HOME/mgireport/$1.rpt
-echo "Date Generated:  `date`" >> $HOME/mgireport/$1.rpt
-echo "(SERVER=$DSQUERY;DATABASE=$MGD)" >> $HOME/mgireport/$1.rpt
-
