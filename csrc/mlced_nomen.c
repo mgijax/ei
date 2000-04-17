ed4c06305e74db9ed54f212464270f8f5775d491
Lori Corbani <Lori.Corbani@jax.org>
2000-04-17 15:17:00 +0000

    TR 1177

--- csrc/mlced_nomen.c
+++ csrc/mlced_nomen.c
@@ -19,9 +19,11 @@
 /*
  * mlced_dbDescToEI
  *
- * Translate given 'txt' string from database by replacing
+ * Translate given 'txt' string by replacing
  * '\L(x)' (where x is the numeric tag) with the appropriate
- * '\L(symbol)' mapping (from the MLC_Marker_edit table).
+ * '\L(symbol)' mapping. 
+ *
+ * Inverse of mlced_eiDescToDB().
  */
 
 char *mlced_dbDescToEI(char *txt, int key)
@@ -54,6 +56,56 @@ char *mlced_dbDescToEI(char *txt, int key)
   }
 
   (void) dbclose(dbproc);
+
+  return newTxt;
+}
+
+/*
+ * mlced_eiDescToDB
+ *
+ * Translate given 'txt' string by replacing
+ * '\L(symbol)' with the appropriate
+ * '\L(x)' (where x is the numeric tag) mapping.
+ *
+ * list contains the list of symbols.
+ *
+ * Inverse of mlced_dbDescToEI().
+ *
+ */
+
+char *mlced_eiDescToDB(char *txt, xrtlist list)
+{
+  static char *newTxt;
+  char chgTxt[TEXTBUFSIZ];
+  char numTag[35];
+  char symTag[35];
+  int numitems;
+  tag_ptr tr;
+  int i = 0;
+  
+  strcpy(chgTxt, txt);
+  numitems = XrtGearListGetItemCount(list);
+
+  /* Iterate thru the list items.
+   * Use the index value of the item in the list plus 1
+   * as the new tag value in the text.
+   *
+   * For example, if list.item[0] = 'Acy1', 
+   * then replace '\L(Acy1)' with '\L(1)'.
+   *
+   * The list will be used in the same way during the creation of
+   * the MLC_Marker_edit table (see MLCED.d/ModifyText)
+   */
+
+  while (i < numitems)
+  {
+    tr = TagList_getitem(list, i);
+    sprintf(numTag, "\\L%c%d%c", OMARKUPCHAR, i + 1, CMARKUPCHAR);
+    sprintf(symTag, "\\L%c%s%c", OMARKUPCHAR, tr->tagstr, CMARKUPCHAR);
+    newTxt = mgi_simplesub(symTag, numTag, chgTxt);
+    strcpy(chgTxt, newTxt);
+    i++;
+  }
 
   return newTxt;
 }
