1da3531bde9bd49db9ce0df3684b4f96f5cb1a97
Jonathan Beal <Jon.Beal@jax.org>
2001-05-31 18:53:43 +0000

    Updated global_version

--- dsrc/MGILib.d
+++ dsrc/MGILib.d
@@ -68,7 +68,7 @@ rules:
 
 	  top := create widget("Login", nil, nil);
 
-	  global_version := "CVS 4-5-0";
+	  global_version := "CVS 4-5-4";
 
 	  SetTitle.source_widget := top;
 	  send(SetTitle, 0);
