47419a827ee8d1512866a2106eb7862fab97857b
Lori Corbani <Lori.Corbani@jax.org>
2002-12-31 18:11:18 +0000

    ei-8-1-1

--- dsrc/MGILib.d
+++ dsrc/MGILib.d
@@ -71,7 +71,7 @@ rules:
 
 	  top := create widget("Login", nil, nil);
 
-	  global_version := "CVS 8-1-0";
+	  global_version := "CVS 8-1-1";
 
 	  SetTitle.source_widget := top;
 	  send(SetTitle, 0);
