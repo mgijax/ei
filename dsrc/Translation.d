d1c92bfcbf83335b4a1885badcb4bf8b42d589fc
Lori Corbani <Lori.Corbani@jax.org>
2003-01-27 16:20:46 +0000

    TR 1463

---
+++ dsrc/Translation.d
@@ -0,0 +1,422 @@
+--
+-- Name    : Translation.d
+-- Creator : 
+-- Translation.d 01/24/2003
+--
+-- TopLevelShell:		TranslationModule
+-- Database Tables Affected:	MGI_Translation
+-- Actions Allowed:		Add, Modify, Delete
+--
+-- To invoke an instance of this module, see MGI.d:CreateMGIModule.
+--
+-- History
+--
+-- 01/24/2003 lec
+--	- SAO; new
+--
+
+dmodule Translation is
+
+#include <mgilib.h>
+#include <syblib.h>
+#include <tables.h>
+
+devents:
+
+	INITIALLY [parent : widget;
+		   launchedFrom : widget;];		-- Initialize form
+	Add :local [];					-- Add record
+	BuildDynamicComponents :local [];		-- Build Dynamic widget components
+	Delete :local [];				-- Delete record
+	Exit :local [];					-- Destroys D module instance & cleans up
+	Init :local [];					-- Initialize globals, etc.
+	Modify :local [];				-- Modify record
+	PrepareSearch :local [];			-- Construct SQL search clause
+	Search :local [prepareSearch : boolean := true;];-- Execute SQL search clause
+	Select :local [item_position : integer;];	-- Select record
+
+locals:
+	mgi : widget;			-- Top-level shell of Application
+	top : widget;			-- Top-level shell of Module
+	ab : widget;			-- Activate Button from whichh this Module was launched
+
+	from : string;			-- global SQL from clause
+	where : string;			-- global SQL where clause
+
+        currentRecordKey : string;      -- Primary Key value of currently selected record
+                                        -- Initialized in Select[] and Add[] events
+ 
+	tables : list;
+
+	transTable : widget;
+
+rules:
+
+--
+-- INITIALLY
+--
+-- Activated from:  MGI:CreateMGIModule
+--
+-- Creates and manages D Module "Translation"
+--
+
+	INITIALLY does
+	  mgi := INITIALLY.parent;
+
+	  (void) busy_cursor(mgi);
+
+	  -- Create the widget hierarchy in memory
+	  top := create widget("TranslationModule", nil, mgi);
+
+          -- Build Dynamic GUI Components
+          send(BuildDynamicComponents, 0);
+ 
+          -- Prevent multiple instances of the form
+	  -- Omit this line to allow multiple instances of forms
+          ab := INITIALLY.launchedFrom;
+          ab.sensitive := false;
+
+	  -- Create windows for all widgets in the widget hierarchy
+	  -- All widgets now visible on screen
+	  top.show;
+
+	  -- Initialize Global variables, Clear form, etc.
+	  send(Init, 0);
+
+	  (void) reset_cursor(mgi);
+	end does;
+
+--
+-- BuildDynamicComponents
+-- (optional)
+--
+-- Activated from:  devent INITIALLY
+--
+-- For initializing dynamic GUI components prior to managing the top form.
+--
+-- Initialize dynamic option menus
+-- Initialize lookup lists
+--
+ 
+        BuildDynamicComponents does
+
+	  InitOptionMenu.option := top->MGITypeMenu;
+	  send(InitOptionMenu, 0);
+
+        end does;
+ 
+--
+-- Init
+--
+-- Activated from:  devent INITIALLY
+--
+-- For initializing static GUI components after managing top form
+-- and global variables.
+--
+-- Initializes global module variables
+-- Sets Row Count
+-- Clears Form
+--
+
+        Init does
+	  tables := create list("widget");
+
+	  -- List of all Table widgets used in form
+
+	  tables.append(top->Translation->Table);
+	  transTable := top->Translation->Table;
+
+          -- Set Row Count
+          SetRowCount.source_widget := top;
+          SetRowCount.tableID := MGI_TRANSLATION;
+          send(SetRowCount, 0);
+ 
+          -- Clear form
+          Clear.source_widget := top;
+          send(Clear, 0);
+
+	end does;
+
+--
+-- Add
+--
+-- Activated from:	top->Control->Add
+--			top->MainMenu->Commands->Add
+--
+-- Construct and execute commands for record insertion
+-- Not used in this module.
+--
+
+        Add does
+	end does;
+
+--
+-- Delete
+--
+-- Activated from:	top->Control->Delete
+--			top->MainMenu->Commands->Delete
+--
+-- Constructs and executes command for record deletion
+-- Not used in this module.
+--
+
+        Delete does
+        end does;
+
+--
+-- Modify
+--
+-- Activated from:	top->Control->Save
+--			top->MainMenu->Commands->Save
+--
+-- Construct and execute command for record modifcations
+--
+-- A unique Translation (MGI_Translation) is defined by its key.
+--
+
+	Modify does
+	  cmd : string;
+          row : integer := 0;
+          editMode : string;
+          transKey : string;
+          objectKey : string;
+	  badName : string;
+	  goodName: string;
+	  currentSeqNum : string;
+	  newSeqNum : string;
+          set : string := "";
+	  keyDeclared : boolean := false;
+	  newTransKey : integer := 1;
+ 
+          if (not top.allowEdit) then
+            return;
+          end if;
+
+	  -- Check for duplicate Seq # assignments
+
+          DuplicateSeqNumInTable.table := transTable;
+          send(DuplicateSeqNumInTable, 0);
+ 
+          if (transTable.duplicateSeqNum) then
+            return;
+          end if;
+ 
+	  (void) busy_cursor(top);
+
+          -- Process while non-empty rows are found
+ 
+          while (row < mgi_tblNumRows(transTable)) do
+            editMode := mgi_tblGetCell(transTable, row, transTable.editMode);
+ 
+            if (editMode = TBL_ROW_EMPTY) then
+              break;
+            end if;
+ 
+            currentSeqNum := mgi_tblGetCell(transTable, row, transTable.currentSeqNum);
+            newSeqNum := mgi_tblGetCell(transTable, row, transTable.seqNum);
+            transKey := mgi_tblGetCell(transTable, row, transTable.transKey);
+            objectKey := mgi_tblGetCell(transTable, row, transTable.objectKey);
+            badName := mgi_tblGetCell(transTable, row, transTable.badName);
+            goodName := mgi_tblGetCell(transTable, row, transTable.goodName);
+ 
+            if (editMode = TBL_ROW_ADD) then
+	      
+	      -- If we need a new Translation key...or if the Translation key
+	      -- was created during this transaction...
+
+	      if (transKey.length = 0 or (integer) transKey < 1000) then
+
+		-- if the key def was not already declared, declare it
+                if (not keyDeclared) then
+                  cmd := cmd + mgi_setDBkey(MGI_TRANSLATION, NEWKEY, KEYNAME);
+                  keyDeclared := true;
+
+		-- if the Translation key is blank, then it's a new Term
+                elsif (transKey.length = 0) then
+                  cmd := cmd + mgi_DBincKey(KEYNAME);
+                end if;
+	      end if;
+
+              cmd := cmd +
+                       mgi_DBinsert(MGI_TRANSLATION, transKey) +
+		       currentRecordKey + "," +
+		       objectKey + "," +
+		       badName + "," + 
+		       newSeqNum + ")\n";
+
+            elsif (editMode = TBL_ROW_MODIFY) then
+              -- If current Seq # not equal to new Seq #, then re-ordering is taking place
+ 
+              if (currentSeqNum != newSeqNum) then
+		set := "sequenceNum = " + newSeqNum;
+              else
+	        set := "_Object_key = " + objectKey + 
+		       ",badName = " + mgi_DBprstr(badName);
+              end if;
+
+              cmd := cmd + mgi_DBupdate(MGI_TRANSLATION, transKey, set);
+
+            elsif (editMode = TBL_ROW_DELETE) then
+               cmd := cmd + mgi_DBdelete(MGI_TRANSLATION, transKey);
+            end if;
+ 
+            row := row + 1;
+	  end while;
+
+          ModifySQL.cmd := cmd;
+	  ModifySQL.list := top->QueryList;
+          send(ModifySQL, 0);
+
+	  (void) reset_cursor(top);
+	end does;
+
+--
+-- PrepareSearch
+--
+-- Construct select statement based on values entered by user
+--
+
+	PrepareSearch does
+	  from := "from " + mgi_DBtable(MGI_TRANSLATIONTYPE) + " ";
+	  where := "";
+	end does;
+
+--
+-- Search
+--
+-- Activated from:	top->Control->Search
+--			top->MainMenu->Commands->Search
+--
+-- Prepare and execute search
+--
+
+	Search does
+
+          (void) busy_cursor(top);
+
+	  if (Search.prepareSearch) then
+	    send(PrepareSearch, 0);
+	  end if;
+
+	  Query.source_widget := top;
+	  Query.select := "select distinct _TranslationType_key, translationType\n" + 
+	  	from + "\n" + where + "\norder by translationType\n";
+	  Query.table := MGI_TRANSLATIONTYPE;
+	  send(Query, 0);
+	  (void) reset_cursor(top);
+	end does;
+
+--
+-- Select
+--
+-- Retrieve and display detail information for specific record
+-- determined by selected row in Query results list.
+--
+
+	Select does
+          (void) busy_cursor(top);
+
+	  tables.open;
+	  while (tables.more) do
+	    ClearTable.table := tables.next;
+	    send(ClearTable, 0);
+	  end while;
+	  tables.close;
+
+          if (top->QueryList->List.selectedItemCount = 0) then
+	    currentRecordKey := "";
+            top->QueryList->List.row := 0;
+            top->ID->text.value := "";
+            (void) reset_cursor(top);
+            return;
+          end if;
+
+	  currentRecordKey := top->QueryList->List.keys[Select.item_position];
+
+	  cmd : string := "select * from " + mgi_DBtable(MGI_TRANSLATIONTYPE) +
+			  " where " + mgi_DBkey(MGI_TRANSLATIONTYPE) + " = " + currentRecordKey +
+			  "select _Translation_key, _Object_key, badName, sequenceNum " +
+			  "from " + mgi_DBtable(MGI_TRANSLATION) +
+			  " where " + mgi_DBkey(MGI_TRANSLATION) + " = " + currentRecordKey +
+			  " order by sequenceNum\n";
+
+	  row : integer := 0;
+	  results : integer := 1;
+          dbproc : opaque := mgi_dbopen();
+          (void) dbcmd(dbproc, cmd);
+          (void) dbsqlexec(dbproc);
+ 
+          while (dbresults(dbproc) != NO_MORE_RESULTS) do
+            while (dbnextrow(dbproc) != NO_MORE_ROWS) do
+	      if (results = 1) then
+	        top->ID->text.value   := mgi_getstr(dbproc, 1);
+	        top->Name->text.value := mgi_getstr(dbproc, 3);
+                SetOption.source_widget := top->MGITypeMenu;
+                SetOption.value := mgi_getstr(dbproc, 2);
+                send(SetOption, 0);
+	      elsif (results = 2) then
+	        (void) mgi_tblSetCell(transTable, row, transTable.transKey, mgi_getstr(dbproc, 1));
+	        (void) mgi_tblSetCell(transTable, row, transTable.objectKey, mgi_getstr(dbproc, 2));
+	        (void) mgi_tblSetCell(transTable, row, transTable.badName, mgi_getstr(dbproc, 3));
+--	        (void) mgi_tblSetCell(transTable, row, transTable.goodName, mgi_getstr(dbproc, 5));
+	        (void) mgi_tblSetCell(transTable, row, transTable.currentSeqNum, mgi_getstr(dbproc, 4));
+	        (void) mgi_tblSetCell(transTable, row, transTable.seqNum, mgi_getstr(dbproc, 4));
+	        (void) mgi_tblSetCell(transTable, row, transTable.editMode, TBL_ROW_NOCHG);
+	      row := row + 1;
+	      end if;
+            end while;
+	    results := results + 1;
+          end while;
+ 
+	  (void) dbclose(dbproc);
+
+	  -- Reset Background
+
+	  -- Stripe rows
+
+	  newBackground : string := transTable.saveBackgroundSeries;
+	  newColor : string := BACKGROUNDNORMAL;
+	  i : integer := 1;
+
+	  while (i < mgi_tblNumRows(transTable)) do
+
+	    -- break when empty row is found
+            if (mgi_tblGetCell(transTable, i, transTable.editMode) = TBL_ROW_EMPTY) then
+	      break;
+	    end if;
+
+	    if (newColor = "Wheat") then
+	      newColor := BACKGROUNDALT1;
+	    else
+	      newColor := BACKGROUNDNORMAL;
+	    end if;
+	    newBackground := newBackground + "(" + (string) i + " all " + newColor + ")";
+	    i := i + 1;
+	  end while;
+
+	  transTable.xrtTblBackgroundSeries := newBackground;
+
+	  -- End Reset Background
+
+          top->QueryList->List.row := Select.item_position;
+
+	  Clear.source_widget := top;
+          Clear.reset := true;
+          send(Clear, 0);
+
+	  (void) reset_cursor(top);
+	end does;
+
+--
+-- Exit
+--
+-- Destroy D module instance and call ExitWindow to destroy widgets
+--
+ 
+        Exit does
+	  ab.sensitive := true;
+          destroy self;
+          ExitWindow.source_widget := top;
+          send(ExitWindow, 0);
+        end does;
+ 
