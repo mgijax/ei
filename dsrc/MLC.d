df6a0cc277582ef8cac5a8c566a31ce3d8619cd5
Lori Corbani <Lori.Corbani@jax.org>
2003-06-12 12:44:21 +0000

    fix clipboard

--- dsrc/MLC.d
+++ dsrc/MLC.d
@@ -635,7 +635,7 @@ rules:
 	  cmd := cmd + mgi_DBinsert(MLC_TEXT, NOKEY) + 
 			currentMarkerKey + ", " + 
 			mgi_DBprstr(top->MLCModeMenu.menuHistory.defaultValue) + "," +
-			mgi_DBprstr(mlced_eiDescToDB(locustxt.value, locustaglist)) + ",";
+			mgi_DBprstr2(mlced_eiDescToDB(locustxt.value, locustaglist)) + ",";
 
 	  -- If a Creation date exists, then save it for the new Text record
 	  -- Else, use the current date
