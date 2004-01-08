c407d9199f63d0482d844574f158f0278a92fd54
Lori Corbani <Lori.Corbani@jax.org>
2004-01-08 14:13:36 +0000

    ei-10-1-4

--- dsrc/MGILib.d
+++ dsrc/MGILib.d
@@ -71,7 +71,7 @@ rules:
 
 	  top := create widget("Login", nil, nil);
 
-	  global_version := "CVS 10-1-3";
+	  global_version := "CVS 10-1-4";
 
 	  SetTitle.source_widget := top;
 	  send(SetTitle, 0);
