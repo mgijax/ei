6747fe25e4b10dbf886c8166905cb9ace103eacc
Lori Corbani <Lori.Corbani@jax.org>
2004-01-20 19:07:45 +0000

    ei-sao-9

--- dsrc/MGILib.d
+++ dsrc/MGILib.d
@@ -72,7 +72,7 @@ rules:
 
 	  top := create widget("Login", nil, nil);
 
-	  global_version := "CVS ei-jsam-8";
+	  global_version := "CVS ei-jsam-9";
 
 	  SetTitle.source_widget := top;
 	  send(SetTitle, 0);
