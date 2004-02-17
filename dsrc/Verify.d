c6f8093e2285be2802ca6933ed7928fa8f96f188
Lori Corbani <Lori.Corbani@jax.org>
2004-02-17 18:38:17 +0000

    JSAM

--- dsrc/Verify.d
+++ dsrc/Verify.d
@@ -376,16 +376,16 @@ rules:
                   else  -- XmOptionMenu
 		    child := editForm.child(i);
 
-		    if (child.is_defined("required") != nil) then
+		    if (child.is_defined("required") != nil and child.is_defined("defaultValue") != nil) then
 		      if (child.required and child.menuHistory.defaultValue = "%") then
 		        -- If Child is required and has a default, use it
 		        if (child.defaultOption = nil) then
-	                    top.allowEdit := false;
-                            StatusReport.source_widget := top;
-                            StatusReport.message := "Required Field \n\n'" + caption + "'";
-                            send(StatusReport);
-	                    (void) XmProcessTraversal(child, XmTRAVERSE_CURRENT);
-		            break;
+	                  top.allowEdit := false;
+                          StatusReport.source_widget := top;
+                          StatusReport.message := "Required Field \n\n'" + caption + "'";
+                          send(StatusReport);
+	                  (void) XmProcessTraversal(child, XmTRAVERSE_CURRENT);
+		          break;
 		        else
 		          child.menuHistory := child.defaultOption;
 		        end if;
