c82ea1ea18fdd860bf4517d308f0470e2e7d9ee1
Lori Corbani <Lori.Corbani@jax.org>
2004-03-04 19:43:19 +0000

    JSAM

--- dsrc/Sequence.d
+++ dsrc/Sequence.d
@@ -327,7 +327,7 @@ rules:
           while (row < mgi_tblNumRows(sourceTable)) do
             editMode := mgi_tblGetCell(sourceTable, row, sourceTable.editMode);
  
-            if (editMode = TBL_ROW_MODIFY and sourceTable.sourceKey != "") then
+            if (editMode = TBL_ROW_MODIFY and mgi_tblGetCell(sourceTable, row, sourceTable.sourceKey) != "") then
               key := mgi_tblGetCell(sourceTable, row, sourceTable.sourceKey);
 	      ModifySequenceSource.source_widget := sourceTable;
 	      ModifySequenceSource.row := row;
