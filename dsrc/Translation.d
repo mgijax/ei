9f14225e8895ba52de6f33819ff52523a16708de
Lori Corbani <Lori.Corbani@jax.org>
2004-06-17 15:16:14 +0000

    TR 5929

--- dsrc/Translation.d
+++ dsrc/Translation.d
@@ -11,6 +11,9 @@
 --
 -- History
 --
+-- 06/17/2004 lec
+--	- TR 5929/revisions; sorting option, searching
+--
 -- 01/24/2003 lec
 --	- SAO; new; aka Bad Name/MGI Term list
 --
@@ -29,12 +32,16 @@ devents:
 	BuildDynamicComponents :local [];		-- Build Dynamic widget components
 	Delete :local [];				-- Delete record
 	Exit :local [];					-- Destroys D module instance & cleans up
+	FindTerm :local [column : integer;];		-- Find MGI or non-MGI Term in table
 	Init :local [];					-- Initialize globals, etc.
 	Modify :local [];				-- Modify record
 	PrepareSearch :local [];			-- Construct SQL search clause
 	Search :local [prepareSearch : boolean := true;];-- Execute SQL search clause
 	Select :local [item_position : integer;];	-- Select record
-	VerifyTransMGITermAccID :local [];			-- Verify MGI Term
+	SetMGIType [];					-- Set MGI Type and DB View
+	SortTable :local [];				-- Sort Rows
+	StripeRows :local [];				-- Strip Rows
+	VerifyTransMGITermAccID :local [];		-- Verify MGI Term
 
 locals:
 	mgi : widget;			-- Top-level shell of Application
@@ -49,8 +56,11 @@ locals:
  
 	tables : list;
 
-	transTable : widget;
+	translationTable : widget;
+        translationTypeKey : string;
+	mgiTypeKey : string;
 	dbView : string;		-- DB View Table (of ACC_MGIType._MGIType_key)
+	abortSearch : boolean := false;
 
 rules:
 
@@ -102,6 +112,9 @@ rules:
  
         BuildDynamicComponents does
 
+	  InitOptionMenu.option := top->TranslationTypeMenu;
+	  send(InitOptionMenu, 0);
+
 	  InitOptionMenu.option := top->MGITypeMenu;
 	  send(InitOptionMenu, 0);
 
@@ -126,7 +139,7 @@ rules:
 	  -- List of all Table widgets used in form
 
 	  tables.append(top->Translation->Table);
-	  transTable := top->Translation->Table;
+	  translationTable := top->Translation->Table;
 
           -- Set Row Count
           SetRowCount.source_widget := top;
@@ -136,6 +149,70 @@ rules:
           -- Clear form
           Clear.source_widget := top;
           send(Clear, 0);
+
+	end does;
+
+--
+-- FindTerm
+--
+-- Search Table for value entered in MGI term or non-MGI term column
+-- Case insensitive
+--
+
+	FindTerm does
+	  searchValue : string;
+	  row : integer := 0;
+	  startRow : integer;
+	  column : integer := FindTerm.column;
+	  editMode : string;
+	  value : string;
+	  foundValue : boolean;
+
+	  searchValue := top->Term->text.value.lower_case;
+
+	  if (searchValue.length = 0) then
+	    return;
+	  end if;
+
+	  row := mgi_tblGetCurrentRow(translationTable);
+	  if (row < 0 or (row + 1) = mgi_tblNumRows(translationTable)) then
+	    row := 0;
+	  else
+	    row := row + 1;
+	  end if;
+	  startRow := row;
+
+	  foundValue := false;
+          while (row < mgi_tblNumRows(translationTable)) do
+            editMode := mgi_tblGetCell(translationTable, row, translationTable.editMode);
+
+            if (editMode = TBL_ROW_EMPTY) then
+	      row := 0;
+            end if;
+ 
+	    value := mgi_tblGetCell(translationTable, row, column).lower_case;
+	    if (strstr(value, searchValue) != nil) then
+	      foundValue := true;
+	    end if;
+
+	    if (foundValue) then
+	      TraverseToTableCell.table := translationTable;
+	      TraverseToTableCell.row := row;
+	      TraverseToTableCell.column := column;
+	      send(TraverseToTableCell, 0);
+	      break;
+	    else
+	      if (row + 1 = mgi_tblNumRows(translationTable)) then
+		row := 0;
+	      else
+	        row := row + 1;
+	      end if;
+
+	      if (row = startRow) then
+		break;
+	      end if;
+	    end if;
+	  end while;
 
 	end does;
 
@@ -181,8 +258,8 @@ rules:
           set : string := "";
           row : integer := 0;
           editMode : string;
-          transKey : string;
           objectKey : string;
+	  transKey : string;
 	  nonmgiTerm : string;
 	  currentSeqNum : string;
 	  newSeqNum : string;
@@ -195,18 +272,14 @@ rules:
 
 	  -- Check for duplicate Seq # assignments
 
-          DuplicateSeqNumInTable.table := transTable;
+          DuplicateSeqNumInTable.table := translationTable;
           send(DuplicateSeqNumInTable, 0);
  
-          if (transTable.duplicateSeqNum) then
+          if (translationTable.duplicateSeqNum) then
             return;
           end if;
  
 	  (void) busy_cursor(top);
-
-	  if (top->Name->text.modified) then
-	    set := set + "translationType = " + mgi_DBprstr(top->Name->text.value) + ",";
-	  end if;
 
 	  if (top->Compression->text.modified) then
 	    set := set + "compressionChars = " + mgi_DBprstr(top->Compression->text.value) + ",";
@@ -218,18 +291,18 @@ rules:
 
           -- Process while non-empty rows are found
  
-          while (row < mgi_tblNumRows(transTable)) do
-            editMode := mgi_tblGetCell(transTable, row, transTable.editMode);
+          while (row < mgi_tblNumRows(translationTable)) do
+            editMode := mgi_tblGetCell(translationTable, row, translationTable.editMode);
  
             if (editMode = TBL_ROW_EMPTY) then
               break;
             end if;
  
-            currentSeqNum := mgi_tblGetCell(transTable, row, transTable.currentSeqNum);
-            newSeqNum := mgi_tblGetCell(transTable, row, transTable.seqNum);
-            transKey := mgi_tblGetCell(transTable, row, transTable.transKey);
-            objectKey := mgi_tblGetCell(transTable, row, transTable.objectKey);
-            nonmgiTerm := mgi_tblGetCell(transTable, row, transTable.nonmgiTerm);
+            currentSeqNum := mgi_tblGetCell(translationTable, row, translationTable.currentSeqNum);
+            newSeqNum := mgi_tblGetCell(translationTable, row, translationTable.seqNum);
+            transKey := mgi_tblGetCell(translationTable, row, translationTable.transKey);
+            objectKey := mgi_tblGetCell(translationTable, row, translationTable.objectKey);
+            nonmgiTerm := mgi_tblGetCell(translationTable, row, translationTable.nonmgiTerm);
  
             if (editMode = TBL_ROW_ADD) then
 	      
@@ -268,6 +341,10 @@ rules:
             row := row + 1;
 	  end while;
 
+	  if (cmd.length > 0) then
+	    cmd := cmd + "\nexec MGI_resetSequenceNum " + mgi_DBprstr(mgi_DBtable(MGI_TRANSLATION)) + "," + translationTypeKey;
+	  end if;
+
           ModifySQL.cmd := cmd;
 	  ModifySQL.list := top->QueryList;
           send(ModifySQL, 0);
@@ -282,8 +359,52 @@ rules:
 --
 
 	PrepareSearch does
-	  from := "from " + mgi_DBtable(MGI_TRANSLATIONTYPE) + " ";
+	  from := "from " + mgi_DBtable(MGI_TRANSLATIONTYPE) + " t";
+	  from_term : boolean := false;
+	  value : string;
+
 	  where := "";
+
+	  abortSearch := false;
+
+          if (top->TranslationTypeMenu.menuHistory.searchValue != "%") then
+	    where := where + "\nand t._TranslationType_key = " + top->TranslationTypeMenu.menuHistory.searchValue;
+	  end if;
+
+          if (top->MGITypeMenu.menuHistory.searchValue != "%") then
+            where := where + "\nand t._MGIType_key = " + mgiTypeKey;
+          end if;
+
+          value := mgi_tblGetCell(translationTable, 0, translationTable.nonmgiTerm);
+	  if (value.length > 0) then
+	    where := where + "\nand m.badName like " + mgi_DBprstr(value);
+	    from_term := true;
+	  end if;
+
+	  -- if searching by mgiTerm, then user has to select the Translation Type
+
+          value := mgi_tblGetCell(top->Translation->Table, 0, top->Translation->Table.mgiTerm);
+	  if (value.length > 0) then
+	    if (mgiTypeKey.length = 0) then
+              StatusReport.source_widget := top.root;
+              StatusReport.message := "If searching by MGI Term, you must specify the MGI Type.";
+              send(StatusReport);
+	      abortSearch := true;
+	      return;
+	    end if;
+	    from_term := true;
+	    where := where + "\nand v.description like " + mgi_DBprstr(value);
+	  end if;
+
+	  if (from_term) then 
+	    from := from + "," + mgi_DBtable(MGI_TRANSLATION) + " m, " + dbView + " v";
+	    where := where + "\nand t._TranslationType_key = m._TranslationType_key";
+	  end if;
+
+	  if (where.length > 0) then
+	    where := "where" + where->substr(5, where.length);
+	  end if;
+
 	end does;
 
 --
@@ -303,11 +424,14 @@ rules:
 	    send(PrepareSearch, 0);
 	  end if;
 
-	  Query.source_widget := top;
-	  Query.select := "select distinct _TranslationType_key, translationType\n" + 
-	  	from + "\n" + where + "\norder by translationType\n";
-	  Query.table := MGI_TRANSLATIONTYPE;
-	  send(Query, 0);
+	  if (not abortSearch) then
+	    Query.source_widget := top;
+	    Query.select := "select distinct t._TranslationType_key, t.translationType\n" + 
+	  	  from + "\n" + where + "\norder by translationType\n";
+	    Query.table := MGI_TRANSLATIONTYPE;
+	    send(Query, 0);
+	  end if;
+
 	  (void) reset_cursor(top);
 	end does;
 
@@ -351,8 +475,10 @@ rules:
           while (dbresults(dbproc) != NO_MORE_RESULTS) do
             while (dbnextrow(dbproc) != NO_MORE_ROWS) do
 	      top->ID->text.value   := mgi_getstr(dbproc, 1);
-	      top->Name->text.value := mgi_getstr(dbproc, 4);
 	      top->Compression->text.value := mgi_getstr(dbproc, 5);
+              SetOption.source_widget := top->TranslationTypeMenu;
+              SetOption.value := mgi_getstr(dbproc, 1);
+              send(SetOption, 0);
               SetOption.source_widget := top->MGITypeMenu;
               SetOption.value := mgi_getstr(dbproc, 2);
               send(SetOption, 0);
@@ -369,7 +495,7 @@ rules:
 		  "from MGI_Translation_View t, " + dbView + " v" +
 		  " where v._Object_key = t._Object_key" + 
 		  " and t._TranslationType_key = " + currentRecordKey +
-		  " order by t.sequenceNum\n";
+		  " order by v.description\n";
 
 	  row : integer := 0;
 
@@ -379,63 +505,111 @@ rules:
           while (dbresults(dbproc) != NO_MORE_RESULTS) do
             while (dbnextrow(dbproc) != NO_MORE_ROWS) do
 
-	      if (row = (integer) mgi_getstr(dbproc, 4) - 1) then
-	        (void) mgi_tblSetCell(transTable, row, transTable.transKey, mgi_getstr(dbproc, 1));
-	        (void) mgi_tblSetCell(transTable, row, transTable.objectKey, mgi_getstr(dbproc, 2));
-	        (void) mgi_tblSetCell(transTable, row, transTable.nonmgiTerm, mgi_getstr(dbproc, 3));
-	        (void) mgi_tblSetCell(transTable, row, transTable.mgiTerm, mgi_getstr(dbproc, 7));
-	        (void) mgi_tblSetCell(transTable, row, transTable.currentSeqNum, mgi_getstr(dbproc, 4));
-	        (void) mgi_tblSetCell(transTable, row, transTable.seqNum, mgi_getstr(dbproc, 4));
-	        (void) mgi_tblSetCell(transTable, row, transTable.modifiedBy, mgi_getstr(dbproc, 5));
-	        (void) mgi_tblSetCell(transTable, row, transTable.modifiedDate, mgi_getstr(dbproc, 6));
-	        (void) mgi_tblSetCell(transTable, row, transTable.editMode, TBL_ROW_NOCHG);
+	      (void) mgi_tblSetCell(translationTable, row, translationTable.transKey, mgi_getstr(dbproc, 1));
+	      (void) mgi_tblSetCell(translationTable, row, translationTable.objectKey, mgi_getstr(dbproc, 2));
+	      (void) mgi_tblSetCell(translationTable, row, translationTable.nonmgiTerm, mgi_getstr(dbproc, 3));
+	      (void) mgi_tblSetCell(translationTable, row, translationTable.mgiTerm, mgi_getstr(dbproc, 7));
+	      (void) mgi_tblSetCell(translationTable, row, translationTable.currentSeqNum, mgi_getstr(dbproc, 4));
+	      (void) mgi_tblSetCell(translationTable, row, translationTable.seqNum, mgi_getstr(dbproc, 4));
+	      (void) mgi_tblSetCell(translationTable, row, translationTable.modifiedBy, mgi_getstr(dbproc, 5));
+	      (void) mgi_tblSetCell(translationTable, row, translationTable.modifiedDate, mgi_getstr(dbproc, 6));
+	      (void) mgi_tblSetCell(translationTable, row, translationTable.editMode, TBL_ROW_NOCHG);
 
-	        if (mgi_getstr(dbproc, 9).length > 0) then
-		  (void) mgi_tblSetCell(transTable, row, transTable.accID, mgi_getstr(dbproc, 9));
-	        else
-		  (void) mgi_tblSetCell(transTable, row, transTable.accID, mgi_getstr(dbproc, 8));
-	        end if;
-
-	        row := row + 1;
+	      if (mgi_getstr(dbproc, 9).length > 0) then
+	        (void) mgi_tblSetCell(translationTable, row, translationTable.accID, mgi_getstr(dbproc, 9));
+	      else
+	        (void) mgi_tblSetCell(translationTable, row, translationTable.accID, mgi_getstr(dbproc, 8));
 	      end if;
+
+	      row := row + 1;
             end while;
           end while;
  
 	  (void) dbclose(dbproc);
 
-	  -- Reset Background
-
-	  -- Stripe rows
-
-	  newBackground : string := transTable.saveBackgroundSeries;
-	  newColor : string := BACKGROUNDNORMAL;
-	  i : integer := 1;
-
-	  while (i < mgi_tblNumRows(transTable)) do
-
-	    -- break when empty row is found
-            if (mgi_tblGetCell(transTable, i, transTable.editMode) = TBL_ROW_EMPTY) then
-	      break;
-	    end if;
-
-	    if (newColor = "Wheat") then
-	      newColor := BACKGROUNDALT1;
-	    else
-	      newColor := BACKGROUNDNORMAL;
-	    end if;
-	    newBackground := newBackground + "(" + (string) i + " all " + newColor + ")";
-	    i := i + 1;
-	  end while;
-
-	  transTable.xrtTblBackgroundSeries := newBackground;
-
-	  -- End Reset Background
+	  send(StripeRows, 0);
 
           top->QueryList->List.row := Select.item_position;
 
 	  Clear.source_widget := top;
           Clear.reset := true;
           send(Clear, 0);
+
+	  (void) reset_cursor(top);
+	end does;
+
+--
+-- SetMGIType
+--
+-- 	Activated from:  MGITypeMenu.toggle.activateCallback
+--
+--	Sets mgiTypeKey, dbView, translationTypeKey globals
+
+        SetMGIType does
+ 
+          if (not SetMGIType.source_widget.set) then
+            return;
+          end if;
+ 
+	  mgiTypeKey := SetMGIType.source_widget.searchValue;
+	  translationTypeKey := top->TranslationTypeMenu.menuHistory.searchValue;
+
+	  if (mgiTypeKey != "%") then
+	    dbView := mgi_sql1("select dbView from ACC_MGIType where _MGIType_key = " + mgiTypeKey);
+	  else
+	    dbView := "";
+	  end if;
+
+	end does;
+
+--
+-- SortTable
+--
+--
+	SortTable does
+	  sortColumn : integer := SortTable.source_widget.columnValue;
+
+          if (not SortTable.source_widget.set) then
+            return;
+          end if;
+ 
+	  (void) mgi_tblSort(translationTable, sortColumn);
+	  send(StripeRows, 0);
+	end does;
+
+--
+-- Stripe Rows
+--
+
+	StripeRows does
+
+	  (void) busy_cursor(top);
+
+	  -- Stripe rows
+
+	  newBackground : string := translationTable.saveBackgroundSeries;
+	  newColor : string := BACKGROUNDALT1;
+	  i : integer := 0;
+
+	  while (i < mgi_tblNumRows(translationTable)) do
+
+	    -- break when empty row is found
+            if (mgi_tblGetCell(translationTable, i, translationTable.editMode) = TBL_ROW_EMPTY) then
+	      break;
+	    end if;
+
+	    if (newColor = BACKGROUNDNORMAL) then
+	      newColor := BACKGROUNDALT1;
+	    else
+	      newColor := BACKGROUNDNORMAL;
+	    end if;
+
+	    newBackground := newBackground + "(" + (string) i + " all " + newColor + ")";
+
+	    i := i + 1;
+	  end while;
+
+	  translationTable.xrtTblBackgroundSeries := newBackground;
 
 	  (void) reset_cursor(top);
 	end does;
@@ -490,6 +664,21 @@ rules:
 	      (void) mgi_tblSetCell(sourceWidget, row, sourceWidget.mgiTerm, "");
 	      (void) mgi_tblSetCell(sourceWidget, row, sourceWidget.accID, "");
 	    end if;
+	    return;
+	  end if;
+
+	  -- If MGI Type is not specified, return
+
+	  if (dbView.length = 0) then
+	    if (isTable) then
+	      VerifyTransMGITermAccID.doit := (integer) false;
+	      (void) mgi_tblSetCell(sourceWidget, row, sourceWidget.objectKey, "NULL");
+	      (void) mgi_tblSetCell(sourceWidget, row, sourceWidget.mgiTerm, "");
+	      (void) mgi_tblSetCell(sourceWidget, row, sourceWidget.accID, "");
+	    end if;
+            StatusReport.source_widget := top.root;
+            StatusReport.message := "If entering a MGI Term, you must first specify the MGI Type.";
+            send(StatusReport);
 	    return;
 	  end if;
 
