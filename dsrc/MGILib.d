61465aebbd8f05252ba1910c56f73435d88b7b0f
Lori Corbani <Lori.Corbani@jax.org>
2005-02-14 14:56:24 +0000

    ei-tr6520-3

--- dsrc/MGILib.d
+++ dsrc/MGILib.d
@@ -83,7 +83,7 @@ rules:
 
 	  top := create widget("Login", nil, nil);
 
-	  global_version := "CVS ei-3-1-1-1";
+	  global_version := "CVS ei-tr6520-3";
 
 	  SetTitle.source_widget := top;
 	  send(SetTitle, 0);
