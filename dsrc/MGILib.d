50cd1dc94f865597d4793083d0c3d8e8efcf1b18
Lori Corbani <Lori.Corbani@jax.org>
2005-04-04 12:46:54 +0000

    ei-mpr-36

--- dsrc/MGILib.d
+++ dsrc/MGILib.d
@@ -83,7 +83,7 @@ rules:
 
 	  top := create widget("Login", nil, nil);
 
-	  global_version := "CVS ei-mpr-35";
+	  global_version := "CVS ei-mpr-36";
 
 	  SetTitle.source_widget := top;
 	  send(SetTitle, 0);
