e7a6d60a3a064c482a157e44a80c5ca0be95de32
Lori Corbani <Lori.Corbani@jax.org>
2005-08-23 11:39:46 +0000

    ei-tr6880-8

--- dsrc/MGILib.d
+++ dsrc/MGILib.d
@@ -83,7 +83,7 @@ rules:
 
 	  top := create widget("Login", nil, nil);
 
-	  global_version := "CVS ei-tr6880-7";
+	  global_version := "CVS ei-tr6880-8";
 
 	  SetTitle.source_widget := top;
 	  send(SetTitle, 0);
