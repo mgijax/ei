2dde12b8d6375b766a2f902b012c11c2d7639488
Lori Corbani <Lori.Corbani@jax.org>
2010-01-27 16:38:31 +0000

    ei-tr9782-3

--- dsrc/MGILib.d
+++ dsrc/MGILib.d
@@ -87,7 +87,7 @@ rules:
 	  top := create widget("Login", nil, nil);
 
 --	  global_version := "CVS ei-TAG_NUMBER";
-	  global_version := "CVS ei-tr9782-2";
+	  global_version := "CVS ei-tr9782-3";
 
 	  SetTitle.source_widget := top;
 	  send(SetTitle, 0);
