50cadc6a30f855bbcb07056ea984e1beeb98dc1a
Lori Corbani <Lori.Corbani@jax.org>
2011-01-25 16:08:31 +0000

    ei-tr10273-4

--- dsrc/MGILib.d
+++ dsrc/MGILib.d
@@ -90,7 +90,7 @@ rules:
 	  top := create widget("Login", nil, nil);
 
 --	  global_version := "CVS ei-TAG_NUMBER";
-	  global_version := "CVS ei-tr10273-3";
+	  global_version := "CVS ei-tr10273-4";
 
 	  SetTitle.source_widget := top;
 	  send(SetTitle, 0);
