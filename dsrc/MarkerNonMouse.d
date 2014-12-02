b9645f7dc4c1c0bc972fa585ca67de4a37dcb135
MGI Admin <mgiadmin@jax.org>
2014-12-02 15:23:54 +0000

    TR11750/postgres

--- dsrc/MarkerNonMouse.d
+++ dsrc/MarkerNonMouse.d
@@ -186,7 +186,7 @@ rules:
 
           -- If adding, then @KEYNAME must be used in all Modify events
  
-          currentRecordKey := "@" + KEYNAME;
+          currentRecordKey := MAX_KEY1 + KEYNAME + MAX_KEY2;
  
 	  -- Insert master Marker Record
 
