1e6696f7cc2878d83f96925d22ce4582f1b96162
Lori Corbani <Lori.Corbani@jax.org>
2015-03-18 14:54:36 +0000

    TR11750/postgres

--- dsrc/MPVocAnnot.d
+++ dsrc/MPVocAnnot.d
@@ -988,10 +988,10 @@ rules:
 	    send(PrepareSearch, 0);
 	  end if;
 
-	  Query.source_widget := top;
-	  Query.select := mpvoc_search(from, where);
-	  Query.table := VOC_ANNOT_VIEW;
-	  send(Query, 0);
+	  QueryNoInterrupt.source_widget := top;
+	  QueryNoInterrupt.select := mpvoc_search(from, where);
+	  QueryNoInterrupt.table := VOC_ANNOT_VIEW;
+	  send(QueryNoInterrupt, 0);
 	  (void) reset_cursor(top);
 	end does;
 
